69389666549ca2b321dd7484a1a574dfa3fd3d10
Marwan Tammam <darknight-x@hotmail.com>
2016-12-25 19:45:23 +0200

    shape alpha

--- src/models/shapes/Shape.java
+++ src/models/shapes/Shape.java
@@ -1,4 +1,14 @@
 package models.shapes;
 
+import java.awt.Color;
+
 public interface Shape {
+    
+    public Color getColor();
+    public void setColor(Color newColor);
+    public int getWidth();
+    public int getHeight();
+
+    
+    
 }
