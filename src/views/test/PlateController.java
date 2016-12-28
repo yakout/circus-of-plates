c9c645afce886b1f42f899df26f1a1e332b1b24a
Mohammed Abdelbarry <m7md337@gmail.com>
2016-12-28 21:37:59 +0200

    Changed PlateController so that it takes a generic type that extends javafx.scene.Node

--- src/views/test/PlateController.java
+++ src/views/test/PlateController.java
@@ -2,23 +2,15 @@ package views.test;
 
 import javafx.animation.Animation;
 import javafx.animation.SequentialTransition;
-import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
-import javafx.beans.property.DoubleProperty;
-import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.shape.Shape;
+import javafx.scene.Node;
 import javafx.util.Duration;
-
-import java.util.Random;
-import java.util.regex.Pattern;
 
 /**
  * Created by Moham on 26-Dec-16.
  */
-public class PlateController<T extends Shape> implements Runnable {
+public class PlateController<T extends Node> implements Runnable {
     private T plate;
     private static final double PLATE_SPEED = 20;
     private static final double PLATE_X_SPEED = 2.3;
@@ -27,12 +19,15 @@ public class PlateController<T extends Shape> implements Runnable {
     private double sign;
     private double rodLength;
     private double offset = 61;
+
     private enum PlateState {
         MOVING,
         IN_STACK,
         INACTIVE
     }
+
     private PlateState state;
+
     public PlateController(T plate, boolean left, double rodLength) {
         this.plate = plate;
         this.rodLength = rodLength;
@@ -48,78 +43,41 @@ public class PlateController<T extends Shape> implements Runnable {
         movingThread.start();*/
     }
 
-    Task<DoubleProperty> moveTask = new Task<DoubleProperty>() {
-        @Override
-        protected DoubleProperty call() throws Exception {
-            /*double center = plate.getParent().getLayoutBounds().getMinX() +
-                    plate
-                            .getParent()
-                            .getLayoutBounds().getWidth() / 2.0;*/
-            boolean fxApplicationThread = Platform.isFxApplicationThread();
-            System.out.println("Is call on FXApplicationThread: " + fxApplicationThread);
-            while (!isCancelled()) {
-                if (plate.getLayoutY() + plate.getTranslateY() >= 500) {
-                    plate.setTranslateX(0);
-                    plate.setTranslateY(0);
-                } else if (Math.abs(350 - (plate.getLayoutX() + 61 / 2.0 +
-                        plate
-                                .getTranslateX() -
-                        sign * PLATE_X_SPEED)) <
-                        61) {
-                    plate.setTranslateY(plate.getTranslateY() +
-                            PLATE_Y_SPEED);
-                } else {
-                    plate.setTranslateX(plate.getTranslateX() + sign *
-                            PLATE_X_SPEED);
-                }
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    System.out.println("Thread (" + Thread.currentThread()
-                            .getName() + ") Interrupted");
-                }
-            }
-            return null;
+    public void move() {
+        TranslateTransition tth = new TranslateTransition(Duration.millis
+                (PLATE_SPEED * STEP * 2), plate);
+        TranslateTransition ttv = new TranslateTransition(Duration.millis
+                (PLATE_SPEED * STEP * 3), plate);
+        tth.setByX(sign * rodLength);
+        ttv.setByY(500);
+        SequentialTransition sq = new SequentialTransition(plate, tth, ttv);
+        sq.setCycleCount(Animation.INDEFINITE);
+        sq.setDelay(Duration.ZERO);
+        if (state == PlateState.MOVING) {
+            sq.play();
+        } else {
+            sq.pause();
         }
-    };
+    }
 
-
-        public void move() {
-            TranslateTransition tth = new TranslateTransition(Duration.millis
-                    (PLATE_SPEED * STEP * 2), plate);
-            TranslateTransition ttv = new TranslateTransition(Duration.millis
-                    (PLATE_SPEED * STEP * 3), plate);
-            tth.setByX(sign * rodLength);
-            ttv.setByY(500);
-            SequentialTransition sq = new SequentialTransition(plate, tth, ttv);
-            sq.setCycleCount(Animation.INDEFINITE);
-            sq.setDelay(Duration.ZERO);
-            if (state == PlateState.MOVING) {
-                sq.play();
-            } else {
-                sq.pause();
-            }
-        }
-
-        @Override
-        public synchronized void run() {
-            double center = plate.getParent().getLayoutBounds().getMinX() +
-                    plate
-                    .getParent()
-                    .getLayoutBounds().getWidth() / 2.0;
-            //       /2.0;
-            // double boundary = center - sign * 61 / 2.0;
-            while (true) {
-                Platform.runLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (plate.getLayoutY() + plate.getTranslateY() >= 500) {
-                            plate.setTranslateX(0);
-                            plate.setTranslateY(0);
-                        } else if (Math.abs(350 - (plate.getLayoutX() + plate
-                                    .getTranslateX() + offset)) < Math.abs(350 -
-                                rodLength)) {
+    @Override
+    public synchronized void run() {
+        double center = plate.getParent().getLayoutBounds().getMinX() +
+                plate
+                        .getParent()
+                        .getLayoutBounds().getWidth() / 2.0;
+        //       /2.0;
+        // double boundary = center - sign * 61 / 2.0;
+        while (true) {
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    if (plate.getLayoutY() + plate.getTranslateY() >= 500) {
+                        plate.setTranslateX(0);
+                        plate.setTranslateY(0);
+                    } else if (Math.abs(350 - (plate.getLayoutX() + plate
+                            .getTranslateX() + offset)) < Math.abs(350 -
+                            rodLength)) {
                             /*Math.abs(350 - (plate.getLayoutX() + 61 /
                                     2.0 +
                                     plate
@@ -127,21 +85,21 @@ public class PlateController<T extends Shape> implements Runnable {
                                     sign * PLATE_SPEED / 7.5)) <
                                     61*/
 
-                            plate.setTranslateY(plate.getTranslateY() +
-                                    PLATE_SPEED / 4);
-                        } else {
-                            plate.setTranslateX(plate.getTranslateX() + sign *
-                                    PLATE_SPEED / 7.5);
-                        }
+                        plate.setTranslateY(plate.getTranslateY() +
+                                PLATE_SPEED / 4);
+                    } else {
+                        plate.setTranslateX(plate.getTranslateX() + sign *
+                                PLATE_SPEED / 7.5);
                     }
-                });
-                try {
-                    this.wait(10);
-                } catch (InterruptedException e) {
-                    System.out.println("Thread (" + Thread.currentThread()
-                            .getName() + ") Interrupted");
-                    break;
                 }
+            });
+            try {
+                this.wait(10);
+            } catch (InterruptedException e) {
+                System.out.println("Thread (" + Thread.currentThread()
+                        .getName() + ") Interrupted");
+                break;
             }
         }
     }
+}
