05dd108b99f2d8925f0febf7b52d835cda76159f
Mohammed Abdelbarry <m7md337@gmail.com>
2017-01-23 15:31:35 +0200

    Added an image fill to the platform

--- src/models/Platform.java
+++ src/models/Platform.java
@@ -3,15 +3,20 @@ package models;
 import javafx.beans.property.DoubleProperty;
 import models.states.Orientation;
 
+import java.io.File;
+import java.net.MalformedURLException;
+
 public class Platform {
 	private Point center;
 	private DoubleProperty width;
 	private DoubleProperty height;
 	private Orientation orientation;
-
+	private static final String URL = "src/assets/images/platforms/rod.png";
+	private String url;
 	public Platform(Point center, Orientation orientation) {
 		this.orientation = orientation;
 		this.center = center;
+		this.url = URL;
 	}
 
 	public void setWidth(DoubleProperty width) {
@@ -42,4 +47,17 @@ public class Platform {
 	}
 	public Point getCenter(){ return center; }
 
+	public String getUrl() {
+		try {
+			return new File(url).toURI().toURL()
+					.toString();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
 }
