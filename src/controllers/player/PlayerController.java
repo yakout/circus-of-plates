f8a0b52561f0a34c760e86ab2e79802f0f850219
Ahmed Yakout <iyakout@hotmail.com>
2017-01-23 15:21:15 +0200

    added stick and player loader methods in player controller

--- src/controllers/player/PlayerController.java
+++ src/controllers/player/PlayerController.java
@@ -1,11 +1,15 @@
 package controllers.player;
 
+import controllers.input.InputType;
 import controllers.main.GameController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 import models.players.PlayerFactory;
+
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,11 +21,16 @@ public class PlayerController {
         players = new HashMap<>();
     }
 
-    public Node createPlayer(String playerName, URL url) throws IOException {
+    public Node createPlayer(String path, String playerName, InputType inputType) throws IOException {
+        URL url = new File(path).toURI().toURL();
         Node player = FXMLLoader.load(url);
         players.put(playerName, player);
+        PlayerFactory.getFactory().registerPlayer(playerName).setInputType(inputType);
         PlayerFactory.getFactory().getPlayer(playerName).setSpeed(0.5); // 5 for primary joystick as it's too fast
                                                                         // 20 is default
+
+        player.setLayoutX(player.getLayoutX() + 500); // TODO add location point to mehtod parameters
+
         return player;
     }
 
@@ -46,4 +55,14 @@ public class PlayerController {
         players.get(playerName).setLayoutX(newX);
     }
 
+    public Node createStick(String path) throws IOException {
+        URL url = new File(path).toURI().toURL();
+        Node plate = FXMLLoader.load(url);
+
+        return plate;
+    }
+
+    public void bindStickWithPlayer(Node player, Node stick) {
+        stick.layoutXProperty().bind(player.layoutXProperty());
+    }
 }
