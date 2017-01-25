bd1d1884b415d4257dbc2667b63de2d307ae1e04
Ahmed Khaled <ahmedkhaled1123300@gmail.com>
2017-01-25 12:47:26 +0200

    rename pot to PotShape

--- src/models/shapes/PotShape.java
+++ src/models/shapes/PotShape.java
@@ -13,23 +13,23 @@ import java.net.MalformedURLException;
 /**
  * Created by Ahmed Khaled on 25/01/2017.
  */
-public class Pot extends Shape {
+public class PotShape extends Shape {
 
     private static final String URL = "src/assets/images/plates/";
-    private static final String FILE_NAME = "Pot.png";
+    private static final String FILE_NAME = "PotShape.png";
     private static final double HORIZONTAL_VELOCITY = 1.8;
     private static final double VERTICAL_VELOCITY = 2.0;
-    private static final String KEY = Pot.class.getName();
+    private static final String KEY = PotShape.class.getName();
     private static Logger logger = LogManager.getLogger();
 
     static {
-        ShapeFactory.registerShape(KEY, Pot.class);
+        ShapeFactory.registerShape(KEY, PotShape.class);
         LevelFour.registerShape(KEY);
         LevelFive.registerShape(KEY);
         logger.debug("Class " + KEY + " initialized");
     }
 
-    public Pot() {
+    public PotShape() {
         super();
         setKey(KEY);
         setHorizontalVelocity(HORIZONTAL_VELOCITY);
